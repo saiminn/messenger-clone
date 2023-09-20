e71a6db2caad9e4f67b41c7ec5df23409f022b40
Sai Minn Khant Kyaw <saiminnkhantkyaw@Sais-MacBook-Air.local>
2023-09-20 21:14:29 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const logInBtn = document.querySelector('.login-button')
+const modelBox = document.querySelector('.model-box')
+
+logInBtn.addEventListener('click',()=>{
+    console.log(modelBox.classList);
+})
